c9c0bd5b64c40b32ab02b0037820a75f18bc0c12
Raphda <61209795+Raphda@users.noreply.github.com>
2022-05-16 13:45:15 +0200

    Update composant_cle.cpp

--- composant_cle/composant_cle.cpp
+++ composant_cle/composant_cle.cpp
@@ -1,54 +1,97 @@
-#include <iostream>
 #include <pybind11/pybind11.h>
 #include "micro-ecc/uECC.h"
-#include "conv-string-bin/convert.cpp"
+char version[]="1.0";
 
-class Cle {
+char const* getVersion() {
+	return version;
+}
 
-	private:
-		// Clé privée
-		uint8_t* private_key;
-		size_t private_key_size;
+uint8_t hexchr2bin(const char hex)
+{
+	uint8_t result;
+
+	if (hex >= '0' && hex <= '9') {
+		result = hex - '0';
+	} else if (hex >= 'A' && hex <= 'F') {
+		result = hex - 'A' + 10;
+	} else if (hex >= 'a' && hex <= 'f') {
+		result = hex - 'a' + 10;
+	} else {
+		return 0;
+	}
+	return result;
+}
+
+
+
+void hexStringToBin(uint8_t *out,const char * hexPrivate) {
+    for (int i=0; i<32; i++){
+	out[i] = hexchr2bin(hexPrivate[2*i])<<4 | hexchr2bin(hexPrivate[2*i+1]);
+    }
+}
+
+
+char *binToHexString(char *out,const unsigned char *bin, size_t len)
+{
+    size_t  i;
+
+    if (bin == NULL || len == 0)
+	return NULL;
+
+    for (i=0; i<len; i++) {
+
+	out[i*2]   = "0123456789abcdef"[bin[i] >> 4];
+	out[i*2+1] = "0123456789abcdef"[bin[i] & 0x0F];
+    }
+    out[len*2] = '\0';
+
+    return out;
+}
+
+
+
+
+
+
+class Cle
+{
+    public:
+        Cle(){}
+        ~Cle() {}
+
+        void initialize(std::string &nb) { 
+		PrivateKey=nb;
+		uint8_t binaryPrivate[32];
+		hexStringToBin(binaryPrivate,PrivateKey.c_str());
+		const int publicKeySize=uECC_curve_public_key_size(uECC_secp256k1());
+		uint8_t *varIntPublicKey = new uint8_t[publicKeySize];
+		uECC_compute_public_key(binaryPrivate,varIntPublicKey,uECC_secp256k1());
+		char hexPublicKey[128];
+		binToHexString(hexPublicKey,varIntPublicKey,64);
+		PublicKey=std::string(hexPublicKey,128);
+		//PublicKey=std::string( varIntPublicKey, varIntPublicKey+publicKeySize );
+		}
 		
-		// Clé publique
-		uint8_t* public_key;
-		size_t public_key_size;
+        const std::string &getPrivateKey() const { return PrivateKey; }
+		const std::string &getPublicKey() const { return PublicKey; }
 
-	public:
-		void initialize(const char* x) {
-
-			// Courbe secp256k1
-			auto curve = uECC_secp256k1();
-
-			// Récupérer la taille
-			private_key_size = uECC_curve_private_key_size(curve);
-			public_key_size = uECC_curve_public_key_size(curve);
-
-			// Créer clé privée
-			private_key = new uint8_t[private_key_size]();
-			hexStringToBin(private_key, x);
-
-			// Générer clé publique à partir de la clé privée
-			public_key = new uint8_t[public_key_size]();
-			uECC_compute_public_key(private_key, public_key, curve);
-		}
-
-		char* getPrivateKey() {
-			return binToHexString(new char[private_key_size](), private_key, private_key_size);
-		}
-		
-		char* getPublicKey() {
-			return binToHexString(new char[public_key_size](), public_key, public_key_size);
-		}
-
+    private:
+        std::string PublicKey;
+        std::string PrivateKey;
 };
-
+ 
 namespace py = pybind11;
 
-PYBIND11_MODULE(composant_cle, cle) {
-	py::class_<Cle>(cle, "cle")
-		.def(py::init<>())
-		.def("initialize", &Cle::initialize)
-		.def("getPrivateKey", &Cle::getPrivateKey)
-		.def("getPublicKey", &Cle::getPublicKey);
+
+PYBIND11_MODULE(composant_cle,greetings)
+{
+  greetings.doc() = "greeting_object 1.0";
+  greetings.def("getVersion", &getVersion, "a function returning the version");
+  
+   // bindings to Cle class
+    py::class_<Cle>(greetings, "Cle", py::dynamic_attr())
+        .def(py::init())
+        .def("initialize", &Cle::initialize)
+        .def("getPrivateKey", &Cle::getPrivateKey)
+        .def("getPublicKey", &Cle::getPublicKey);
 }
